4496d35a4dfdd10501f58d66249b36d64ba90af3
Morgan H. McKie <m.mckie1@umiami.edu>
2024-04-22 16:23:40 -0400

    Update Intro.js

--- docs/Intro.js
+++ docs/Intro.js
@@ -4,7 +4,9 @@ function Intro() {
     
   }
   this.draw = function() {
+      //load blue background color for the sky
       background(color(0,100,190));
+      //load background image 02 and 01. Order in which they are called is important.  
       image(this.sceneManager.bgImg02,-450,70);
       image(this.sceneManager.bgImg01,-450,70);
       
@@ -13,7 +15,7 @@ function Intro() {
       //load player with controlls 
       player0.create(curPlayerState);
       
-      
+      //draw different game sprites
       drawSprites();
       
       
